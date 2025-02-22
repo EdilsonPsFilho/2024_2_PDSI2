d1148b379cabbdbdf7816c562b633e493e0bdc3a
EdilsonPsFilho <edilsonpsfilho@gmail.com>
2025-02-21 21:59:36 -0300

    Alterações no database

--- database.py
+++ database.py
@@ -1,10 +1,14 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-user = "postgres"
-password = "145512"
-database = "data_ufu"
-host = "localhost"
+load_dotenv()
+
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+database = os.getenv("DB_NAME")
+host = os.getenv("DB_HOST")
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}/{database}"
 
